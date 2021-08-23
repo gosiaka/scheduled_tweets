213624f753591704b2dddcd0cb6475422df79d11
Gosia <malgorzata.kska@gmail.com>
2021-08-23 16:39:29 +0200

    Create Twitter Account model connected with User & Twitter

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,21 @@
 class OmniauthCallbacksController < ApplicationController
   def twitter
+    Rails.logger.info auth   # print out info in Rails logs
 
+    twitter_account = Current.user.twitter_accounts.where(username: auth.info.nickname).first_or_initialize
+    twitter_account.update(
+      name: auth.info.name,
+      username: auth.info.nickname,
+      image: auth.info.image,
+      token: auth.credentials.token,
+      secret: auth.credentials.secret,
+    )
+
+    redirect_to root_path, notice: "Successfully connected your account"
+  end
+
+  def auth
+    # give us a hash of credentials from twitter
+    request.env['omniauth.auth']
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@
 
 class User < ApplicationRecord
 
+  has_many :twitter_accounts
   has_secure_password
 
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/,  message: "Must be a valid email adress"}
